96c0c247a3ffe680bdf99298f01efc6d681d4c2d
Jeremy Heffner <jerheff@gmail.com>
2014-08-11 18:34:31 -0400

    Add Proxy settings to config template

--- R/configuration/config-template.R
+++ R/configuration/config-template.R
@@ -1,5 +1,33 @@
 # SERVER
+
+# server url [string]
 config.server = "https://us.hunchlab.com"
 
-# TOKEN
+# PROXY
+
+# IP or URL of proxy [string]
+#     No scheme suggest HTTP proxy (e.g. "192.168.1.1")
+#     Other options: socks4:// socks4a:// socks5://
+#
+# config.proxy.url = ""
+
+# proxy port [number]
+# config.proxy.port =  
+
+# proxy username [string]
+# config.proxy.username = 
+
+# proxy password [string]
+# config.proxy.password
+
+# CREDENTIALS
+
+# API token found in admin interface [string]
 config.token = "YOURTOKEN"
+
+# DATASERVICE
+
+# coordinate system EPSG [number]
+#    4326 is lat/lng
+#    3857 is web mercator
+config.dataservice.srid = 4326
